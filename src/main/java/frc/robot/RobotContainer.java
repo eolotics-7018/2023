b3bf7d079a3930335a6975b722f36fbaa6592c48
Epic_Alberto <edyal2222@gmail.com>
2023-01-14 15:24:06 -0600

    otros archivos

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Train;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final CommandXboxController mStick = new CommandXboxController(Constants.OperatorConstants.KPControl);
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Train s_DriveTrain = new Train();
+  private final Conveyor s_Conveyor = new Conveyor();
+  
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+    
+    // s_DriveTrain.setDefaultCommand(new RunCommand(()->s_DriveTrain.Drive(mStick.getLeftY(), mStick.getRightX()), s_DriveTrain));
+    s_Conveyor.setDefaultCommand(new RunCommand(()->s_Conveyor.beltMove(0.0), s_Conveyor));
+    s_DriveTrain.setDefaultCommand(new RunCommand(()-> s_DriveTrain.Drive(mStick.getLeftY(), mStick.getRightX()), s_DriveTrain));
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+
+    // mStick.rightTrigger().onTrue(new RunCommand(()->s_Conveyor.beltMove(mStick.getRightTriggerAxis()), s_Conveyor));
+    mStick.leftTrigger().whileTrue(new RunCommand(()->s_Conveyor.beltMove(-mStick.getLeftTriggerAxis()), s_Conveyor));
+  }
+  
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Train.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Train extends SubsystemBase {
+  /** Creates a new Train. */
+  private final WPI_VictorSPX MLF = new WPI_VictorSPX(Constants.OperatorConstants.kPMLF);
+  private final WPI_VictorSPX MLR = new WPI_VictorSPX(Constants.OperatorConstants.kPMLR);
+  private final WPI_VictorSPX MRF = new WPI_VictorSPX(Constants.OperatorConstants.kPMRF);
+  private final WPI_VictorSPX MRR = new WPI_VictorSPX(Constants.OperatorConstants.kPMRR);
+  private final MotorControllerGroup mLeft = new  MotorControllerGroup(MLF, MLR) ;
+  private final MotorControllerGroup mRight = new MotorControllerGroup(MRF, MRR);
+  public DifferentialDrive mDrive = new DifferentialDrive(mLeft, mRight);
+  public Train() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public void Drive(double ySpeed, double xSpeed){
+    mDrive.arcadeDrive(ySpeed, xSpeed);
+  }
+
+  public void moverUno(double speed) {
+    MRR.set(ControlMode.PercentOutput, speed);
+  }
+}
