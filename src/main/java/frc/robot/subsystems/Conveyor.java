9ca88a5df84b6f121825989e2fabb959019bcd5f
Epic_Alberto <edyal2222@gmail.com>
2023-01-21 13:15:22 -0600

    se agrego el motor de arriba de la banda

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -13,20 +13,26 @@ import frc.robot.Constants;
 
 public class Conveyor extends SubsystemBase {
   /** Creates a new Conveyor. */
-  private final CANSparkMax mBelt = new CANSparkMax(Constants.OperatorConstants.kPBelt,MotorType.kBrushless);
+  private final CANSparkMax mBelt = new CANSparkMax(Constants.OperatorConstants.kPBelt, MotorType.kBrushless);
+  private final CANSparkMax mBeltUp = new CANSparkMax(Constants.OperatorConstants.kPBeltUp, MotorType.kBrushless);
 
-  // private SparkMaxPIDController pid_controller = mBelt.getPIDController();
+  private boolean beltUpOn = true;
+
   private RelativeEncoder encoder;
   private double motorSpeedProportion = 1;
 
   public Conveyor() {
     mBelt.restoreFactoryDefaults();
-  
+    mBeltUp.restoreFactoryDefaults();
+
     encoder = mBelt.getEncoder();
   }
 
   public void beltMove(double speed){
     mBelt.set(speed * motorSpeedProportion);
+    if (beltUpOn) {
+      mBeltUp.set(speed * motorSpeedProportion);
+    }
   }
 
   public void faster() {
@@ -39,6 +45,10 @@ public class Conveyor extends SubsystemBase {
     if (motorSpeedProportion > 0) {
       motorSpeedProportion -= 0.05;
     }
+  }
+
+  public void toggleBeltUp() {
+    beltUpOn = !beltUpOn;
   }
 
   public RelativeEncoder getEncoder() {
